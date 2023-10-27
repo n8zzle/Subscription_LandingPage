9a1d90446b8ab5e063af98cde92f4c02bc577fc0
n8zzle <playwithn8zzle@gmail.com>
2023-10-27 17:30:26 +0200

    Adding Navigation bar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="w-full h-full">
-      <h1>Hello, World</h1>
+    <main className="w-full h-full px-3 md:px-10 md:py-5">
+      <Navbar />
     </main>
   );
 }
