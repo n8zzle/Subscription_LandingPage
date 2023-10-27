0207a0d28c8dc590da4114bc59ef2d7421299e77
n8zzle <playwithn8zzle@gmail.com>
2023-10-28 01:31:25 +0200

    changed base style settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="w-full h-full px-3 md:px-10 md:py-5">
+    <main className="w-full p-5 bg-white">
       <Navbar />
+      <Hero />
     </main>
   );
 }
